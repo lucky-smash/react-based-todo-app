6953b4a1ffd7119d71182e95cda81468ab66b261
Lakshit taygi <tyagilakshit6@gmail.com>
2025-11-29 18:14:53 +0530

    Initial commit - React ToDo App

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar.jsx'
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <div className="flex justify-between bg-red-100" >
+      <h1>Hello, World!</h1>
+</div>
+    </>
+  )
+}
+
+export default App
