a9bafd59a582f33337c774f8df65a18ccf02c2c0
Lakshit taygi <tyagilakshit6@gmail.com>
2025-12-04 03:46:04 +0530

    edit save delete functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,60 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Navbar from './components/Navbar.jsx'
 import { v4 as uuidv4 } from 'uuid' ;
-import useRef from 'react' ;
+
 
 function App() {
 
   const [todo, setTodo] = useState(" ")
   const [todos, setTodos] = useState([])
-  const [useref, setUseref] = useState(null)
+
+  useEffect(() => {
+    let todos = JSON.parse(localStorage.getItem("todos")) 
+    setTodos(todos) ;
+  }, [])
+
+
+  const saveToLs = (params) => {
+    localStorage.setItem("todos" , JSON.stringify(todos)) ;
+  }
 
   useState(() => {
     alert("work in progress")
   })
 
-  const handleEdit = () => {
-    alert("Edit button clicked")
+  const handleEdit = (e,id) => {
+    console.log(`The id is ${id}`) ;
+    let t = todos.filter(i=>i.id === id)
+    setTodo(t[0].todo) ;
+    let newTodos = todos.filter(item =>{
+      return item.id!==id ;
+    });
+    setTodos([...newTodos]) ;
+    saveToLs() ;
   }
 
-  const handleDelete = () => {
-    alert("Delete button clicked")
+  const handleDelete = (e ,id) => {
+    console.log(`The id is ${id}`) ;
+    let index = todos.findIndex(item => {
+      return item.id === item.id ;
+    })
+    let newTodos = todos.filter(item =>{
+      return item.id!==id ;
+
+    });
+    setTodos([...newTodos]) ;
+    saveToLs() ;
+
   }
   const handleAdd = () => {
     setTodos([...todos, { id: uuidv4() , todo, isCompleted: false }])
     setTodo(" ")
-    console.log(todos);
+    console.log(todos); 
   }
   const handleChange = (e) => {
     setTodo(e.target.value)
+    
+    saveToLs() ;
 
   }
 
@@ -41,8 +69,7 @@ function App() {
     let newTodos = [...todos] ;
     newTodos[index].isCompleted = !newTodos[index].isCompleted ;
     setTodos([...newTodos]) ;
-    //
-    // todos.filter()
+    saveToLs() ;
   }
 
   return (
@@ -56,23 +83,25 @@ function App() {
 
           <input value={todo} onChange={handleChange} type="text" className="bg-white m-3 w-3/4" />
 
-          <button onClick={handleAdd} className="bg-violet-800 hover:bg-violet-950 p-3 py-0 text-white rounded-md">Add</button>
+          <button onClick={handleAdd} className="bg-violet-800 hover:bg-violet-950 p-3 py-0 text-white rounded-md">Save</button>
         </div>
 
         <h1 className='text-lg rounded-lg '>Your Todos</h1>
 
         <div className="todos m-10">
+          {todos.length === 0 && <div>No todos to display</div>}
 
           {todos.map((item, index) => {
 
             return <div key={index} className="todo flex justify-between my-3">
 
+            <div className="flex gap-5">
               <input name = {item.id} onChange={handleCheckBox} type="checkbox" value={item.isCompleted} id = " "/>
-              <div className={item.isCompleted ? "line-through" : ""}>{item.todo}</div>
+              <div className={item.isCompleted ? "line-through" : ""}>{item.todo}</div></div>
 
-              <div className="buttons">
-                <button onClick={handleEdit} className="bg-violet-800 hover:bg-violet-950 p-3 py-0 text-white rounded mx-1">Edit</button>
-                <button onClick={handleDelete} className="bg-violet-800 hover:bg-violet-950 p-3 py-0 text-white rounded mx-1">DELETE</button>
+              <div className="buttons flex ">
+                <button onClick={(e)=>{handleEdit(e,item.id)}} className="bg-violet-800 hover:bg-violet-950 p-3 py-0 text-white rounded mx-1">Edit</button>
+                <button onClick={(e)=>{handleDelete(e, item.id)}} className="bg-violet-800 hover:bg-violet-950 p-3 py-0 text-white rounded mx-1">DELETE</button>
               </div>
             </div>
           })}
